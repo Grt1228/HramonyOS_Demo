00ec6e10e91bfd811d3a93bdc65d02835f3b0075
guorutao <guorutao@sundear.com>
2020-12-25 17:32:55 +0800

    3

--- entry/src/main/js/default/pages/index/index.js
+++ entry/src/main/js/default/pages/index/index.js
@@ -15,11 +15,63 @@ export default {
         flag: true,
         date: null,
         time: null,
+        emoji: [{
+                    "path": "../common/1-1.png",
+                    "selectPath": "../common/1.png",
+                    "id": 1
+                },
+                {
+                    "path": "../common/2-2.png",
+                    "selectPath": "../common/2.png",
+                    "id": 2
+                },
+                {
+                    "path": "../common/3-3.png",
+                    "selectPath": "../common/3.png",
+                    "id": 3
+                }
+        ],
+        imgOne: "https://gw.alicdn.com/tps/TB1W_X6OXXXXXcZXVXXXXXXXXXX-400-400.png",
+        imgTwo: "../common/2-2.png",
+        imgThree: "../common/3-3.png",
         cartText: 'Add To Cart',
         cartStyle: 'cart-text',
         isCartEmpty: true,
         descriptionFirstParagraph: 'This is the food page including fresh fruit, meat, snack and etc. You can pick whatever you like and add it to your Cart. Your order will arrive within 48 hours. We gurantee that our food is organic and healthy. Feel free to ask our 24h online service to explore more about our platform and products.',
         imageList: ['/common/food_000.JPG', '/common/food_001.JPG', '/common/food_002.JPG', '/common/food_003.JPG'],
+    },
+    changeFace(index){
+        if(index == 1){
+            this.imgOne = "../common/1.png";
+        }else if(index == 2){
+            this.imgTwo = "../common/2.png";
+        }else{
+            this.imgThree = "../common/3.png";
+        }
+    },
+    changeDate: function(val){
+        val.month = val.month +1;
+        if (val.month >= 0 && val.month <= 9) {
+            val.month = "0" + val.month;
+        }
+        if (val.day >= 0 && val.day <= 9) {
+            val.day = "0" + val.day;
+        }
+        this.date = val.year + "-" + val.month + "-" + val.day
+    },
+    changeTime: function(val){
+        if (val.minute >= 0 && val.minute <= 9) {
+            val.minute = "0" + val.minute;
+        }
+        if (val.hour >= 0 && val.hour <= 9) {
+            val.hour = "0" + val.hour;
+        }
+        this.time = val.hour + ":" + val.minute
+    },
+    history: function(){
+        router.push ({
+            uri: 'pages/detail/detail',
+        });
     },
     submit: function() {
         this.$element('dialog').show()
@@ -44,9 +96,7 @@ export default {
 //            }
 //        })
 
-//        router.push ({
-//            uri: 'pages/detail/detail',
-//        });
+
     },
     getSum: function () {
         console.log("222222222222222222222222222222")
@@ -105,8 +155,36 @@ export default {
             this.cartStyle = 'cart-text';
         }
     },
+    getNowFormatDateTime() {
+        var date = new Date();
+        var seperatorDate = "-";
+        var year = date.getFullYear();
+        var month = (date.getMonth() + 1).toString();
+        var strDate = (date.getDate()).toString();
+        if (month >= 1 && month <= 9) {
+            month = "0" + month;
+        }
+        if (strDate >= 0 && strDate <= 9) {
+            strDate = "0" + strDate;
+        }
+        var currentdate = year + seperatorDate + month + seperatorDate + strDate;
+        this.date = currentdate;
+        var hour = date.getHours().toString();//得到小时
+        var minu = date.getMinutes().toString();//得到分钟
+        var sec = date.getSeconds();//得到秒
+        var seperatorTime = ":"
+        if (minu >= 0 && minu <= 9) {
+            minu = "0" + minu;
+        }
+        if (hour >= 0 && hour <= 9) {
+            hour = "0" + hour;
+        }
+        var currentTime = hour + seperatorTime + minu ;
+        this.time = currentTime;
+    },
+
     onInit(){
-        this.date = new Date().;
-        this.time = new Date();
+        console.log("------------------"+this.emoji.toString())
+        this.getNowFormatDateTime();
     }
 }
